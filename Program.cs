14c1b30f1f97cd6319733482aa696143140ca1d5
maheir <maheirne@gmail.com>
2018-12-18 15:08:25 +0100

    EXERCICE 1 TABLEAU

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tableau_1bis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] jours = new string[] { "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" };
+
+            for (int i = 0; i < jours.Length; i++)
+
+            {
+
+                Console.WriteLine(jours[i]);
+
+            }
+        }
+    }
+}
